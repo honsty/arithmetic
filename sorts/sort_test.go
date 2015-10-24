785627a37c4688380d138aa07c7b31df5a5e03b9
honsty <honsty@qq.com>
2015-10-24 17:02:17 +0800

    README.md

--- sorts/sort_test.go
+++ sorts/sort_test.go
@@ -5,5 +5,22 @@ import (
 )
 
 func TestBubbleSort(t *testing.T) {
+	data := []int{50, 10, 30, 20, 40}
+	t.Logf("排序前%#v", data)
+	BubbleSort(data)
+	t.Logf("冒泡排序后%#v", data)
+}
 
+func TestQuickSort(t *testing.T) {
+	data := []int{20, 40, 50, 10, 60}
+	t.Logf("排序前：%#v", data)
+	QuickSort(data, 0, 4)
+	t.Logf("快速排序后%#v", data)
+}
+
+func TestSelectSort(t *testing.T) {
+	data := []int{20, 40, 50, 10, 60}
+	t.Logf("排序前：%#v", data)
+	SelectionSort(data)
+	t.Logf("选择排序后%#v", data)
 }
